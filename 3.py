105089d80e4ad7ba18b767a84cd2f6afb10b3e17
zhouy <773858927@qq.com>
2017-04-14 15:59:43 +0800

    selenium打开百度等待10秒，打开新闻，等待2秒，返回百度

---
+++ 3.py
@@ -0,0 +1,19 @@
+from selenium import webdriver
+
+import time
+
+driver = webdriver.Firefox()
+
+#访问百度首页
+first_url = 'http://www.baidu.com'
+print("now access %s"%first_url)
+driver.get(first_url)
+time.sleep(10)
+#访问新闻页面
+second_url = 'http://news.baidu.com'
+print("now access %s"%second_url)
+driver.get(second_url)
+time.sleep(2)
+#后退到百度首页
+print("back to %s"%first_url)
+driver.back()
