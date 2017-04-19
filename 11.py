b30d84704529b2ec4bc121ea92a955fee857ff09
zhouy <773858927@qq.com>
2017-04-19 09:22:34 +0800

    selenium谷歌浏览器对话框处理

--- 11.py
+++ 11.py
@@ -1,13 +1,15 @@
 from selenium import webdriver
-
-driver = webdriver.Firefox()
+import time
+driver = webdriver.Chrome()
 driver.get("http://www.baidu.com")
 
 #点击登录
-driver.find_element_by_name("tj_login").click()
-
+driver.find_element_by_xpath("/html/body/div[2]/div[1]/div/div[3]/a[7]").click()
+time.sleep(2)
+sreach_window=driver.current_window_handle #此行代码用来定位当前页面
+time.sleep(1)
 #通过二次定位找到用户名输入框
-div = driver.find_element_by_class_name("tang-content").find_element_by_name("userName")
+div = driver.find_element_by_id("TANGRAM__PSP_2__content").find_element_by_name("userName")
 div.clear()
 div.send_keys("username")
 #输入登录密码
