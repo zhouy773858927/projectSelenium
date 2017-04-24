aaaee8c4da5e40e30df48d405b679e8bf5e62838
zhouy <773858927@qq.com>
2017-04-24 15:55:38 +0800

    selenium  验证码cookie问题；有问题

---
+++ 19.py
@@ -0,0 +1,21 @@
+from selenium import webdriver
+import time
+
+driver = webdriver.Chrome()
+driver.get("http://192.168.0.186:6008/saferycom/login.do")
+coolie = driver.get_cookies()
+print(coolie)
+time.sleep(10)
+#将用户名密码写入浏览器 cookie
+driver.add_cookie({'name':'login_name','value':'admin'})
+driver.add_cookie({'name':'login_password','value':'saferycom'})
+coolie = driver.get_cookies()
+print(coolie)
+time.sleep(5)
+#再次访问 xxxx 网站，将会自动登录
+driver1 = webdriver.Chrome()
+driver1.get("http://192.168.0.186:6008/saferycom/com_index.do")
+coolie = driver.get_cookies()
+print(coolie)
+
+time.sleep(2)
