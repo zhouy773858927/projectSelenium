5e3b7853cec8720c463d365049cc1850a90f7d96
zhouy <773858927@qq.com>
2017-04-26 11:47:58 +0800

    selenium 测试嵌套提交

---
+++ test_case/all_tests.py
@@ -0,0 +1,25 @@
+import unittest
+import HTMLTestRunner
+import baidu,youdaoyongli
+
+# 定义一个单元测试容器
+testunit = unittest.TestSuite()
+
+# 将测试用例加入测试容器中
+testunit.addTest(unittest.makeSuite(baidu.Baidu))
+testunit.addTest(unittest.makeSuite(youdaoyongli.Youdao))
+
+# 定义报告存放路径
+filename = 'E:\\requestsSelenium\\request.html'
+fp = open(filename, 'wb')
+
+# 定义测试报告
+runner = HTMLTestRunner.HTMLTestRunner(
+    stream=fp,
+    title=u'百度搜索测试报告',
+    description=u'用例执行情况'
+)
+
+# 执行用例
+runner.run(testunit)
+fp.close()
