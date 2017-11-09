61374ecb90a582d37e8edc673eda7815edde973d
zhouy <773858927@qq.com>
2017-11-09 15:02:22 +0800

    截图处理，目前有点bug

---
+++ shanghai/Jietu.py
@@ -0,0 +1,47 @@
+from selenium import webdriver
+import unittest
+import os,sys,time
+import HTMLTestRunner
+
+# #登录
+# driver =webdriver.Ie()
+# current_time = time.strftime("%Y-%m-%d-%H_%M_%S", time.localtime(time.time()))
+# current_time1 = time.strftime("%Y-%m-%d", time.localtime(time.time()))
+# print(current_time )
+# print(current_time1 )
+# # 必须打印图片路径HTMLTestRunner才能捕获并且生成路径，\image\**\\**.png 是获取路径的条件,必须这样的目录
+# #设置存储图片路径，测试结果图片可以按照每天进行区分
+#
+# #通过if进行断言判断
+# driver.get("https://baidu.com/")
+# pic_path = 'C:\\Users\\Administrator\\Desktop\\'+ current_time1+'\\' + current_time +'.png'
+# print(pic_path)
+# time.sleep(5)
+# print(driver.title)
+# #截取当前url页面的图片，并将截取的图片保存在指定的路劲下面（pic_path），注：一下两种方法都可以
+# driver.save_screenshot(pic_path)
+# driver.save_screenshot('C:\\Users\\Administrator\\Desktop\\'+ current_time1+'\\' + current_time +'.png')
+#
+# if u'百度一下，你就知道' == driver.title:
+#     print ('Assertion test pass.')
+# else:
+#      print ('Assertion test fail.')
+#
+#   #通过try抛出异常进行断言判断
+# driver.get("https://baidu.com/")
+# driver.save_screenshot(pic_path)
+# try:
+#     assert  u'百度一下，你就知道' ==  driver.title
+#     print ('Assertion test pass.')
+# except Exception as e:
+#     print ('Assertion test fail.', format(e))
+#
+# time.sleep(5)
+# driver.quit()
+def jietu(self):
+    driver = self.driver
+    current_time = time.strftime("%Y-%m-%d-%H_%M_%S", time.localtime(time.time()))
+    print(current_time )
+    time.sleep(3)
+    j = driver.get_screenshot_as_file("C:\\Users\\Administrator\\Desktop\\jietu\\"+current_time+".png")
+    print(j)
--- shanghai/main.py
+++ shanghai/main.py
@@ -1,9 +1,11 @@
-from selenium import webdriver
 import time
-from selenium.webdriver.support.ui import WebDriverWait
-from shanghai import login
 import unittest
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from shanghai import login, Jietu
 
+
+#流程：一级稽核通过-->二级稽核不通过-->一级稽核不通过-->稽核整改
 class Main(unittest.TestCase):
     def setUp(self):
         self.driver = webdriver.Ie()
@@ -28,12 +30,15 @@ class Main(unittest.TestCase):
         driver.switch_to_frame("tabId_1301")
         #工单号
         element = WebDriverWait(driver, 10).until(lambda driver: self.driver.find_element_by_xpath("//*[@id='searchDocId']"))
-        element.send_keys("1508901930344")
-        time.sleep(1)
+        element.send_keys("1508309831931")
+        time.sleep(2)
         #搜索按钮
         element = WebDriverWait(driver, 10).until(
             lambda driver: self.driver.find_element_by_xpath("/html/body/div[1]/div/div/div[2]/div[1]/div[2]/span[8]/span/a/span"))
         element.click()
+        #调用截图方法
+        Jietu.jietu(self)
+        time.sleep(1)
         # element=WebDriverWait(driver, 10).until(lambda driver :driver.find_element_by_xpath("//div[@id='menu']/div[9]/div[2]/ul/li[3]"))
         #查看按钮
         element = WebDriverWait(driver, 10).until(lambda driver: self.driver.find_element_by_xpath(
@@ -45,6 +50,10 @@ class Main(unittest.TestCase):
             if handle1 != oldhandle:
                 driver.switch_to_window(handle1)
                 print("it is now handle1")
+                time.sleep(10)
+                # 调用截图方法（目前此处截图黑屏）
+                Jietu.jietu(self)
+                time.sleep(1)
                 #稽核通过按钮
                 element = WebDriverWait(driver, 10).until(
                     lambda driver: self.driver.find_element_by_xpath("//a[@id='auditButtonYes']/span/span"))
@@ -54,6 +63,7 @@ class Main(unittest.TestCase):
                     lambda driver: self.driver.find_element_by_xpath("/html/body/div[11]/div[2]/div[2]/a[1]/span/span"))
                 element.click()
                 driver.close()
+        #关闭标签页
         time.sleep(1)
         #转到旧句柄
         driver.switch_to_window(oldhandle)
@@ -66,11 +76,11 @@ class Main(unittest.TestCase):
         #工单号
         element = WebDriverWait(driver, 10).until(
             lambda driver: self.driver.find_element_by_xpath("//*[@id='searchDocId']"))
-        element.send_keys("1508901930344")
-        time.sleep(1)
+        element.send_keys("1508309831931")
+        time.sleep(2)
         #搜索按钮
         element = WebDriverWait(driver, 10).until(
-            lambda driver: self.driver.find_element_by_xpath("/html/body/div[1]/div/div/div[2]/div[1]/div[2]/span[8]/span/a/span"))
+            lambda driver: self.driver.find_element_by_xpath("/html/body/div[1]/div/div/div[2]/div[1]/div[2]/div[3]/span[1]/a/span/span"))
         element.click()
         #查看按钮
         #element=WebDriverWait(driver, 10).until(lambda driver :driver.find_element_by_xpath("//div[@id='menu']/div[9]/div[2]/ul/li[3]"))
@@ -103,15 +113,108 @@ class Main(unittest.TestCase):
                 element = WebDriverWait(driver, 10).until(
                     lambda driver: self.driver.find_element_by_xpath("//*[@id='reason2']"))
                 element.send_keys("ck")
+                time.sleep(1)
                 #确定按钮
+                ##########bug：定位不到提交按钮
                 element = WebDriverWait(driver, 10).until(
-                    lambda driver: self.driver.find_element_by_xpath("/html/body/div[8]/div[2]/div[2]/a[1]/span"))
+                    lambda driver: self.driver.find_element_by_xpath("/html/body/div[8]/div[2]/div[2]/a[1]/span/span"))
                 element.click()
+                # case = driver.find_element_by_xpath("//*[@id='audit_state']")
+                # print(case.text)
+                time.sleep(6)
                 driver.close()
         driver.switch_to_window(oldhandle)
-
-
-
+        time.sleep(5)
+        ##############有bug：二级稽核已经不通过了，但是一级稽核进去时候提示已通过
+        # 一级稽核界面
+        element = WebDriverWait(driver, 10, 0.5).until(
+            lambda driver: self.driver.find_element_by_xpath("//li[@id='一级稽核']"))
+        # element=WebDriverWait(driver, 10, 0.5).until(lambda driver :driver.find_element_by_xpath("//div[9]/div[2]/ul/li[3]"))
+        element.click()
+        time.sleep(1)
+        driver.switch_to_frame("tabId_1301")
+        # 搜索按钮
+        element = WebDriverWait(driver, 10).until(
+            lambda driver: self.driver.find_element_by_xpath(
+                "/html/body/div[1]/div/div/div[2]/div[1]/div[2]/span[8]/span/a/span"))
+        element.click()
+        # element=WebDriverWait(driver, 10).until(lambda driver :driver.find_element_by_xpath("//div[@id='menu']/div[9]/div[2]/ul/li[3]"))
+        # 查看按钮
+        element = WebDriverWait(driver, 10).until(lambda driver: self.driver.find_element_by_xpath(
+            "/html/body/div[1]/div/div/div[2]/div[2]/div[2]/div[2]/table/tbody/tr/td[12]/div/a"))
+        element.click()
+        allhandles = driver.window_handles
+        # 判断当前窗口句柄
+        for handle3 in allhandles:
+            if handle3 != oldhandle:
+                driver.switch_to_window(handle3)
+                print("it is now handle3")
+                # 一级稽核不通过按钮
+                element = WebDriverWait(driver, 10).until(
+                    lambda driver: self.driver.find_element_by_xpath("/html/body/div[2]/div/div[4]/a[2]/span/span"))
+                element.click()
+                # 差错类型下拉框
+                element = WebDriverWait(driver, 10).until(
+                    lambda driver: self.driver.find_element_by_xpath(
+                        "/html/body/div[5]/div[2]/div[1]/div/form/div[3]/span/span/span"))
+                element.click()
+                time.sleep(1)
+                # 下拉框选择
+                element = WebDriverWait(driver, 10).until(
+                    lambda driver: self.driver.find_element_by_xpath("/html/body/div[11]/div/ul/li[3]/div/span[3]"))
+                element.click()
+                element = WebDriverWait(driver, 10).until(
+                    lambda driver: self.driver.find_element_by_xpath(
+                        "/html/body/div[5]/div[2]/div[1]/div/form/div[3]/span/span/span"))
+                element.click()
+                # 备注
+                element = WebDriverWait(driver, 10).until(
+                    lambda driver: self.driver.find_element_by_xpath("//*[@id='reason2']"))
+                element.send_keys("ck")
+                # 确定按钮
+                element = WebDriverWait(driver, 10).until(
+                    lambda driver: self.driver.find_element_by_xpath("/html/body/div[5]/div[2]/div[2]/a[1]/span"))
+                element.click()
+                driver.close()
+        time.sleep(1)
+        # 转到旧句柄
+        driver.switch_to_window(oldhandle)
+        #稽核整改界面
+        element = WebDriverWait(driver ,10).until(
+            lambda driver:self.driver.find_element_by_xpath("//*[@id='稽核整改']")
+        )
+        element.click()
+        driver.switch_to_frame("tabId_1305")
+        #工单号
+        element = WebDriverWait(driver, 10).until(
+            lambda driver: self.driver.find_element_by_xpath("//*[@id='searchDocId']"))
+        element.send_keys("1508309831931")
+        time.sleep(2)
+        #搜索按钮
+        element = WebDriverWait(driver, 10).until(
+            lambda driver: self.driver.find_element_by_xpath("/html/body/div[1]/div/div/div[2]/div[1]/div/span[7]/span/a/span"))
+        element.click()
+        #查看按钮
+        element = WebDriverWait(driver, 10).until(
+            lambda driver: self.driver.find_element_by_xpath(
+                "/html/body/div[1]/div/div/div[2]/div[2]/div[2]/div[2]/table/tbody/tr/td[12]/div/a"))
+        element.click()
+        driver.switch_to_frame("businessfile")
+        #整改表单按钮
+        element = WebDriverWait(driver, 10).until(
+            lambda driver: self.driver.find_element_by_xpath(
+                "/html/body/div[2]/div/div[4]/a[1]/span/span"))
+        element.click()
+        #整改备注
+        element = WebDriverWait(driver, 10).until(
+            lambda driver: self.driver.find_element_by_xpath(
+                "//*[@id='reason1']"))
+        element.send_keys("sk")
+        #提交按钮
+        element = WebDriverWait(driver, 10).until(
+            lambda driver: self.driver.find_element_by_xpath(
+                "/html/body/div[6]/div[2]/div[2]/a[1]/span"))
+        element.click()
 if __name__ == '__main__':
     unittest.main()
 
