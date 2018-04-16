99885c1b93525f16e528ea131da9e451bd31cf9e
zhouy <773858927@qq.com>
2018-04-16 16:07:42 +0800

    优化禅道登录脚本（由于之前运行后cpu占用率过高，本次优化每一小时获取一次本地时间，并且在和判断进行校验）

--- chandaologin2.py
+++ chandaologin2.py
@@ -4,13 +4,14 @@ from selenium import webdriver
 
 def func():
     #登录禅道方法
-    driver = webdriver.Ie()
+    driver = webdriver.Chrome()
     driver.implicitly_wait(10)
-    driver.get("http://192.168.0.193/zentao/user-login.html")
+
+    driver.get("http://192.168.0.193/zentao")
     #验证网页是否正常打开，不正常则抛出异常，但不影响程序运行
     try:
         assert '用户登录 - 禅道' ==  driver.title
-        print("Assertion test pass.")
+        print("Assertion test pass.",time.strftime('%Y-%m-%d %H:%M:%S'))
     except Exception as e:
         print('Assertion test fail.', format(e))
     driver.find_element_by_id("account").send_keys("zhouyang")
@@ -22,8 +23,11 @@ def func():
 while True:
     #获取当前计算机时间
     current_time = time.localtime(time.time())
-    #约定9点30分0秒运行func方法
-    if ((current_time.tm_hour == 9) and (current_time.tm_min == 30) and (current_time.tm_sec == 0)):
+    print(current_time)
+    #每1小时获取次本地计算机时间
+    time.sleep(3600)
+    #约定每天14时一小时内校检一次
+    if ((18<current_time.tm_hour<=19 ) and (0<=current_time.tm_min<=60) and (0<=current_time.tm_sec<=60)):
         func()
-        print("登录成功",current_time)
+        print("禅道在%s时登录成功" %time.strftime('%Y-%m-%d %H:%M:%S'))
         time.sleep(1)
